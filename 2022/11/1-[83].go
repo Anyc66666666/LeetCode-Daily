8f9c3cc2d6f041d39ae980b6f1c4610d9b8d84b1
Anyc <1662048992@qq.com>
2022-11-01 18:06:19 +0800

    2022/11/11-[83]

---
+++ 2022/11/1-[83].go
@@ -0,0 +1,75 @@
+package leetcode
+
+//给定一个已排序的链表的头
+// head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,1,2]
+//输出：[1,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点数目在范围 [0, 300] 内
+// -100 <= Node.val <= 100
+// 题目数据保证链表已经按升序 排列
+//
+//
+// Related Topics 链表 👍 880 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	var m []int
+	n := 666
+	for head != nil {
+		if head.Val != n {
+			m = append(m, head.Val)
+			n = head.Val
+		}
+		head = head.Next
+
+	}
+	var x = new(ListNode)
+	xx := x
+	xx.Val = 666
+
+	for i := 0; i < len(m); i++ {
+		xx.Next = &ListNode{
+			Val:  m[i],
+			Next: nil,
+		}
+		xx = xx.Next
+	}
+
+	return x.Next
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
