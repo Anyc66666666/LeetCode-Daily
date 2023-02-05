2d339224cf6c7bcdce609b47a1076abf3eb7a7c0
Anyc66666666 <1662048992@qq.com>
2023-02-05 22:31:25 +0800

    💬2023/2/5-[551]

---
+++ 2023/2/5-[551].go
@@ -0,0 +1,80 @@
+package leetcode
+
+//给你一个字符串 s 表示一个学生的出勤记录，其中的每个字符用来标记当天的出勤情况（缺勤、迟到、到场）。记录中只含下面三种字符：
+//
+//
+// 'A'：Absent，缺勤
+// 'L'：Late，迟到
+// 'P'：Present，到场
+//
+//
+// 如果学生能够 同时 满足下面两个条件，则可以获得出勤奖励：
+//
+//
+// 按 总出勤 计，学生缺勤（'A'）严格 少于两天。
+// 学生 不会 存在 连续 3 天或 连续 3 天以上的迟到（'L'）记录。
+//
+//
+// 如果学生可以获得出勤奖励，返回 true ；否则，返回 false 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "PPALLP"
+//输出：true
+//解释：学生缺勤次数少于 2 次，且不存在 3 天或以上的连续迟到记录。
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "PPALLL"
+//输出：false
+//解释：学生最后三天连续迟到，所以不满足出勤奖励的条件。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 1000
+// s[i] 为 'A'、'L' 或 'P'
+//
+//
+// Related Topics 字符串 👍 141 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func checkRecord(s string) bool {
+	var absence int
+	var late, c bool
+	var b int
+	for i := 0; i < len(s); i++ {
+		if late && s[i] == 'L' {
+			b++
+			if b >= 2 {
+				c = true
+			}
+			continue
+		}
+		if s[i] == 'A' {
+			absence++
+			late = false
+			b = 0
+			continue
+		}
+		if s[i] == 'L' {
+			late = true
+			continue
+		}
+		late = false
+		b = 0
+	}
+
+	return !c && absence < 2
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
