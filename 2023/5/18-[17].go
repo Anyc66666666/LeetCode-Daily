c3ae98a33c3333ee137ea77f19155e18920a73c3
Anyc66666666 <1662048992@qq.com>
2023-05-19 00:00:43 +0800

    💬2023/5/18-[17]

---
+++ 2023/5/18-[17].go
@@ -0,0 +1,78 @@
+package leetcode
+
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：digits = "23"
+//输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+//
+//
+// 示例 2：
+//
+//
+//输入：digits = ""
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：digits = "2"
+//输出：["a","b","c"]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= digits.length <= 4
+// digits[i] 是范围 ['2', '9'] 的一个数字。
+//
+//
+// Related Topics 哈希表 字符串 回溯 👍 2463 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func letterCombinations(digits string) []string {
+	var str []string
+	m := map[byte][]string{
+		'2': {"a", "b", "c"},
+		'3': {"d", "e", "f"},
+		'4': {"g", "h", "i"},
+		'5': {"j", "k", "l"},
+		'6': {"m", "n", "o"},
+		'7': {"p", "q", "r", "s"},
+		'8': {"t", "u", "v"},
+		'9': {"w", "x", "y", "z"},
+	}
+
+	for i := 0; i < len(digits); i++ {
+		if i == 0 {
+			str = m[digits[i]]
+			continue
+		}
+		str = help(str, m[digits[i]])
+
+	}
+	return str
+
+}
+func help(a, b []string) []string {
+	str := make([]string, 0, len(a)*len(b))
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(b); j++ {
+			str = append(str, a[i]+b[j])
+		}
+	}
+	return str
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
