aee3d360ddca63cf42d2bb9727696ce4a02e8696
Anyc66666666 <1662048992@qq.com>
2023-05-27 23:44:18 +0800

    💬2023/5/27-[28]

---
+++ 2023/5/27-[28.].go
@@ -0,0 +1,54 @@
+package leetcode
+
+//给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
+//如果 needle 不是 haystack 的一部分，则返回 -1 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：haystack = "sadbutsad", needle = "sad"
+//输出：0
+//解释："sad" 在下标 0 和 6 处匹配。
+//第一个匹配项的下标是 0 ，所以返回 0 。
+//
+//
+// 示例 2：
+//
+//
+//输入：haystack = "leetcode", needle = "leeto"
+//输出：-1
+//解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= haystack.length, needle.length <= 10⁴
+// haystack 和 needle 仅由小写英文字符组成
+//
+//
+// Related Topics 双指针 字符串 字符串匹配 👍 1872 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func strStr(haystack string, needle string) int {
+	length := len(needle)
+	l := len(haystack)
+	for i := 0; i < l; i++ {
+
+		if i+length > l {
+			return -1
+		}
+		if haystack[i:i+length] == needle {
+			return i
+		}
+	}
+
+	return -1
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
