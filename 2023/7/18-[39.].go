9451af0375e01bc4ec697c69349ec2ab473e9c9c
Anyc66666666 <1662048992@qq.com>
2023-07-18 23:08:30 +0800

    💬2023/7/18-[39.]

---
+++ 2023/7/18-[39.].go
@@ -0,0 +1,80 @@
+package leetcode
+
+//给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的
+// 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+//
+// candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+//
+// 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：candidates = [2,3,6,7], target = 7
+//输出：[[2,2,3],[7]]
+//解释：
+//2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+//7 也是一个候选， 7 = 7 。
+//仅有这两种组合。
+//
+// 示例 2：
+//
+//
+//输入: candidates = [2,3,5], target = 8
+//输出: [[2,2,2,2],[2,3,3],[3,5]]
+//
+// 示例 3：
+//
+//
+//输入: candidates = [2], target = 1
+//输出: []
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= candidates.length <= 30
+// 2 <= candidates[i] <= 40
+// candidates 的所有元素 互不相同
+// 1 <= target <= 40
+//
+//
+// Related Topics 数组 回溯 👍 2539 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func combinationSum(candidates []int, target int) [][]int {
+	var ans [][]int
+	var comb []int
+	var dfs func(target, idx int)
+	dfs = func(target, idx int) {
+		if idx == len(candidates) {
+			return
+		}
+		if target == 0 {
+			ans = append(ans, append([]int(nil), comb...))
+			return
+		}
+		// 直接跳过
+		dfs(target, idx+1)
+		// 选择当前数
+		if target-candidates[idx] >= 0 {
+			comb = append(comb, candidates[idx])
+			dfs(target-candidates[idx], idx)
+			comb = comb[:len(comb)-1]
+		}
+	}
+	dfs(target, 0)
+	return ans
+
+	//作者：LeetCode-Solution
+	//链接：https://leetcode.cn/problems/combination-sum/solution/zu-he-zong-he-by-leetcode-solution/
+	//来源：力扣（LeetCode）
+	//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
