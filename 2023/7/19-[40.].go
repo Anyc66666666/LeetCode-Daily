c4519c7b75d9d0b217074a88c07dd6e2efad56d9
Anyc66666666 <1662048992@qq.com>
2023-07-20 09:56:27 +0800

    💬2023/7/19-[40.]

--- 2023/7/19-[40.].go
+++ 2023/7/19-[40.].go
@@ -48,45 +48,39 @@ import (
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func combinationSum2(candidates []int, target int) [][]int {
-	sort.Ints(candidates)
-	var ans [][]int
-	var freq [][2]int
-	for _, num := range candidates {
-		if freq == nil || num != freq[len(freq)-1][0] {
-			freq = append(freq, [2]int{num, 1})
-		} else {
-			freq[len(freq)-1][1]++
-		}
-	}
+	length := len(candidates)
+	res, path := make([][]int, 0), make([]int, 0, length)
+	sort.Ints(candidates) //排序，为剪枝做准备
+	var dfs func(start, target int)
 
-	var sequence []int
-	var dfs func(pos, rest int)
-	dfs = func(pos, rest int) {
-		if rest == 0 {
-			ans = append(ans, append([]int(nil), sequence...))
+	dfs = func(start, target int) {
+		if target == 0 { //达到目标值,path符合题意
+			//但不能把path直接追加，因为后续还要用，不然底层会修改
+			tmp := make([]int, len(path))
+			copy(tmp, path)
+			res = append(res, tmp)
 			return
 		}
-		if pos == len(freq) || rest < freq[pos][0] {
-			return
+		// 1 1 2 2 5 6 7 10
+		for i := start; i < length; i++ {
+			if candidates[i] > target { //剪枝 提前返回
+				//不然target变负数了，不符合题意
+				return
+			}
+
+			if i != start && candidates[i] == candidates[i-1] {
+				continue //去重
+			}
+			path = append(path, candidates[i])
+			dfs(i+1, target-candidates[i])
+			path = path[:len(path)-1]
+
 		}
 
-		dfs(pos+1, rest)
-
-		//most := min1(rest/freq[pos][0], freq[pos][1])
-		var most int
-		if rest/freq[pos][0] < freq[pos][1] {
-			most = rest / freq[pos][0]
-		} else {
-			most = freq[pos][1]
-		}
-		for i := 1; i <= most; i++ {
-			sequence = append(sequence, freq[pos][0])
-			dfs(pos+1, rest-i*freq[pos][0])
-		}
-		sequence = sequence[:len(sequence)-most]
 	}
+
 	dfs(0, target)
-	return ans
+	return res
 
 }
 
@@ -98,6 +92,46 @@ func combinationSum2(candidates []int, target int) [][]int {
 //	}
 //	return b
 //}
+
+//sort.Ints(candidates)
+//var ans [][]int
+//var freq [][2]int
+//for _, num := range candidates {
+//if freq == nil || num != freq[len(freq)-1][0] {
+//freq = append(freq, [2]int{num, 1})
+//} else {
+//freq[len(freq)-1][1]++
+//}
+//}
+
+//var sequence []int
+//var dfs func(pos, rest int)
+//dfs = func(pos, rest int) {
+//	if rest == 0 {
+//		ans = append(ans, append([]int(nil), sequence...))
+//		return
+//	}
+//	if pos == len(freq) || rest < freq[pos][0] {
+//		return
+//	}
+//
+//	dfs(pos+1, rest)
+//
+//	//most := min1(rest/freq[pos][0], freq[pos][1])
+//	var most int
+//	if rest/freq[pos][0]<freq[pos][1]{
+//		most=rest/freq[pos][0]
+//	}else {
+//		most=freq[pos][1]
+//	}
+//	for i := 1; i <= most; i++ {
+//		sequence = append(sequence, freq[pos][0])
+//		dfs(pos+1, rest-i*freq[pos][0])
+//	}
+//	sequence = sequence[:len(sequence)-most]
+//}
+//dfs(0, target)
+//return ans
 
 //作者：LeetCode-Solution
 //链接：https://leetcode.cn/problems/combination-sum-ii/solution/zu-he-zong-he-ii-by-leetcode-solution/
