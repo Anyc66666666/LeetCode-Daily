cb67c3342a27c742434b432a609d4fead436faad
Anyc66666666 <1662048992@qq.com>
2023-07-26 23:46:45 +0800

    💬2023/7/26-[78.]

---
+++ 2023/7/26-[78].go
@@ -0,0 +1,58 @@
+package leetcode
+
+//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+//
+// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,2,3]
+//输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [0]
+//输出：[[],[0]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+// nums 中的所有元素 互不相同
+//
+//
+// Related Topics 位运算 数组 回溯 👍 2096 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func subsets(nums []int) [][]int {
+	var ans [][]int
+	var a []int
+	var dfs func(now int)
+	dfs = func(now int) {
+		m := make([]int, len(a))
+		copy(m, a)
+		ans = append(ans, m)
+
+		for i := now; i < len(nums); i++ {
+			a = append(a, nums[i])
+			dfs(i + 1)
+			a = a[:len(a)-1]
+		}
+	}
+
+	dfs(0)
+
+	return ans
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
