2337af32b97ff3e124fa7cd283e28ddfedc14a5b
Anyc66666666 <1662048992@qq.com>
2023-08-29 23:57:42 +0800

    💬2023/8/29-[416..]

--- 2023/8/27-[416.].go
+++ 2023/8/27-[416.].go
@@ -32,51 +32,71 @@ package leetcode
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func canPartition(nums []int) bool {
-	n := len(nums)
-	if n < 2 {
-		return false
-	}
-	sum, max := 0, 0
+	//n := len(nums)
+	//if n < 2 {
+	//	return false
+	//}
+	//sum, max := 0, 0
+	//
+	//for _, v := range nums {
+	//	sum += v
+	//
+	//	if v > max {
+	//		max = v
+	//	}
+	//
+	//}
+	//
+	//if sum%2 != 0 {
+	//	return false
+	//}
+	//
+	//target := sum / 2
+	//
+	//if max > target {
+	//	return false
+	//}
+	//
+	//dp := make([][]bool, n)
+	//for i := range dp {
+	//	dp[i] = make([]bool, target+1)
+	//}
+	//for i := 0; i < n; i++ {
+	//	dp[i][0] = true
+	//}
+	//dp[0][nums[0]] = true
+	//for i := 1; i < n; i++ {
+	//	v := nums[i]
+	//	for j := 1; j <= target; j++ {
+	//		if j >= v {
+	//			dp[i][j] = dp[i-1][j] || dp[i-1][j-v]
+	//		} else {
+	//			dp[i][j] = dp[i-1][j]
+	//		}
+	//	}
+	//}
+	//
+	//return dp[n-1][target]
 
+	//------
+	var sum int
 	for _, v := range nums {
 		sum += v
-
-		if v > max {
-			max = v
-		}
-
 	}
-
 	if sum%2 != 0 {
 		return false
 	}
+	sum = sum / 2
 
-	target := sum / 2
-
-	if max > target {
-		return false
-	}
-
-	dp := make([][]bool, n)
-	for i := range dp {
-		dp[i] = make([]bool, target+1)
-	}
-	for i := 0; i < n; i++ {
-		dp[i][0] = true
-	}
-	dp[0][nums[0]] = true
-	for i := 1; i < n; i++ {
-		v := nums[i]
-		for j := 1; j <= target; j++ {
-			if j >= v {
-				dp[i][j] = dp[i-1][j] || dp[i-1][j-v]
-			} else {
-				dp[i][j] = dp[i-1][j]
+	dp := make([]int, sum+1) //all 0
+	for _, num := range nums {
+		for j := sum; j >= num; j-- {
+			if dp[j] < dp[j-num]+num {
+				dp[j] = dp[j-num] + num
 			}
 		}
 	}
-
-	return dp[n-1][target]
+	return dp[sum] == sum
 
 }
 
