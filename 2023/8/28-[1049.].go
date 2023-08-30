00850a6b6268441536880d22ec8cd8f457d3d989
Anyc66666666 <1662048992@qq.com>
2023-08-30 23:46:16 +0800

    💬2023/8/30-[1049..]

--- 2023/8/28-[1049.].go
+++ 2023/8/28-[1049.].go
@@ -46,35 +46,85 @@ package leetcode
 //leetcode submit region begin(Prohibit modification and deletion)
 
 func lastStoneWeightII(stones []int) int {
-	sum := 0
+	//sum := 0
+	//for _, v := range stones {
+	//	sum += v
+	//}
+	//n, m := len(stones), sum/2
+	//dp := make([][]bool, n+1)
+	//for i := range dp {
+	//	dp[i] = make([]bool, m+1)
+	//}
+	//dp[0][0] = true
+	//for i, weight := range stones {
+	//	for j := 0; j <= m; j++ {
+	//		if j < weight {
+	//			dp[i+1][j] = dp[i][j]
+	//		} else {
+	//			dp[i+1][j] = dp[i][j] || dp[i][j-weight]
+	//		}
+	//	}
+	//}
+	//for j := m; ; j-- {
+	//	if dp[n][j] {
+	//		return sum - 2*j
+	//	}
+	//}
+	//
+	////作者：力扣官方题解
+	////链接：https://leetcode.cn/problems/last-stone-weight-ii/solutions/817930/zui-hou-yi-kuai-shi-tou-de-zhong-liang-i-95p9/
+	////来源：力扣（LeetCode）
+	////著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+	//-------------
+
+	var sum int
 	for _, v := range stones {
 		sum += v
 	}
-	n, m := len(stones), sum/2
-	dp := make([][]bool, n+1)
-	for i := range dp {
-		dp[i] = make([]bool, m+1)
-	}
-	dp[0][0] = true
-	for i, weight := range stones {
-		for j := 0; j <= m; j++ {
-			if j < weight {
-				dp[i+1][j] = dp[i][j]
-			} else {
-				dp[i+1][j] = dp[i][j] || dp[i][j-weight]
-			}
-		}
-	}
-	for j := m; ; j-- {
-		if dp[n][j] {
-			return sum - 2*j
+	m := sum / 2
+	dp := make([]int, m+1)
+	for _, stone := range stones {
+		for j := m; j >= stone; j-- {
+			dp[j] = max3(dp[j], dp[j-stone]+stone)
 		}
 	}
 
-	//作者：力扣官方题解
-	//链接：https://leetcode.cn/problems/last-stone-weight-ii/solutions/817930/zui-hou-yi-kuai-shi-tou-de-zhong-liang-i-95p9/
-	//来源：力扣（LeetCode）
-	//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+	return sum - 2*dp[m]
+}
+
+func max3(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+
+/*
+// 0.1 如何计算：
+// 我们可以先求出这一堆石头的总质量sum，
+// 而sum = heap1 + heap2    （heap1 > heap2）
+// heap1 - heap2 = sum - 2 * heap2
+// 要求heap1 - heap2 的最小值，就可以转化成求sum - 2 * heap2 的最小值，
+// 也就转化成了求 2 * heap2 的最大值，也就是求heap2的最大值（前提：sum - 2 * heap2 >= 0 等价于 heap2 <= sum / 2）
+// 那么就转化成了01背包问题：背包的最大容量为 sum / 2
+
+
+// 1.状态表示：f[i][j]
+// 1.1 集合划分：i表示前i个石头，j表示背包最大容量；f[i][j]就表示前i个数，容量为j的背包能装下的石头最大质量
+// （注意：此题的石头体积与石头质量都是stones[i]）
+// 1.2 属性（一般为最大值、最小值、数量）：最大值
+
+
+// 2.状态计算：
+// 2.1 f[i][j]可以表示成f[i - 1][j]，表示不加上当前第i块石头的背包最大质量
+// 2.2 f[i][j]还可以表示成f[i - 1][j - stones[i]] + stones[i]，表示加上第i块石头后背包最大质量
+// 2.3 所以最后总和为j的所有方案为：f[i][j] = max(f[i - 1][j] + f[i - 1][j - stones[i]] + stones[i])
+
+
+// 3.优化：
+// 通过滚动数组可以将二维数组优化成一维数组：f[i][j] --> f[j]
+
+*/
