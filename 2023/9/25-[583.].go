a216ac128ab5be506e74a24539a86a17521df23f
Anyc66666666 <1662048992@qq.com>
2023-09-25 22:44:47 +0800

    💬2023/9/25-[583.]

---
+++ 2023/9/25-[583.].go
@@ -0,0 +1,72 @@
+package leetcode
+
+//给定两个单词 word1 和
+// word2 ，返回使得
+// word1 和
+// word2 相同所需的最小步数。
+//
+// 每步 可以删除任意一个字符串中的一个字符。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入: word1 = "sea", word2 = "eat"
+//输出: 2
+//解释: 第一步将 "sea" 变为 "ea" ，第二步将 "eat "变为 "ea"
+//
+//
+// 示例 2:
+//
+//
+//输入：word1 = "leetcode", word2 = "etco"
+//输出：4
+//
+//
+//
+//
+// 提示：
+//
+//
+//
+// 1 <= word1.length, word2.length <= 500
+// word1 和 word2 只包含小写英文字母
+//
+//
+// Related Topics 字符串 动态规划 👍 626 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func minDistance(word1 string, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(word2)+1)
+	}
+	//init
+	for i := 0; i < len(dp); i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j < len(dp[0]); j++ {
+		dp[0][j] = j
+	}
+
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[i]); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min11(min11(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1]+2)
+			}
+		}
+	}
+	return dp[len(dp)-1][len(dp[0])-1]
+}
+
+func min11(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
