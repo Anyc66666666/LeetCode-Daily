4db2bc35625b1b3bc19635b1b9ed7e090610e6ee
Anyc66666666 <1662048992@qq.com>
2023-09-26 23:12:35 +0800

    💬2023/9/26-[72.]

---
+++ 2023/9/26-[72.].go
@@ -0,0 +1,85 @@
+package leetcode
+
+//给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数 。
+//
+// 你可以对一个单词进行如下三种操作：
+//
+//
+// 插入一个字符
+// 删除一个字符
+// 替换一个字符
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：word1 = "horse", word2 = "ros"
+//输出：3
+//解释：
+//horse -> rorse (将 'h' 替换为 'r')
+//rorse -> rose (删除 'r')
+//rose -> ros (删除 'e')
+//
+//
+// 示例 2：
+//
+//
+//输入：word1 = "intention", word2 = "execution"
+//输出：5
+//解释：
+//intention -> inention (删除 't')
+//inention -> enention (将 'i' 替换为 'e')
+//enention -> exention (将 'n' 替换为 'x')
+//exention -> exection (将 'n' 替换为 'c')
+//exection -> execution (插入 'u')
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= word1.length, word2.length <= 500
+// word1 和 word2 由小写英文字母组成
+//
+//
+// Related Topics 字符串 动态规划 👍 3153 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func minDistance2(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+	dp := make([][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 0; i < m+1; i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j < n+1; j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min23(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+			}
+
+		}
+	}
+	return dp[m][n]
+}
+func min23(a ...int) int {
+	min3 := a[0]
+	for _, v := range a {
+		if v < min3 {
+			min3 = v
+		}
+	}
+	return min3
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
