e5e8df845610e9525d31a4697345dde1d060ab12
Anyc66666666 <1662048992@qq.com>
2023-10-07 23:26:00 +0800

    💬2023/10/7-[42.]

---
+++ 2023/10/7-[42.].go
@@ -0,0 +1,64 @@
+package leetcode
+
+//给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+//输出：6
+//解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+//
+//
+// 示例 2：
+//
+//
+//输入：height = [4,2,0,3,2,5]
+//输出：9
+//
+//
+//
+//
+// 提示：
+//
+//
+// n == height.length
+// 1 <= n <= 2 * 10⁴
+// 0 <= height[i] <= 10⁵
+//
+//
+// Related Topics 栈 数组 双指针 动态规划 单调栈 👍 4759 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func trap(height []int) int {
+	var stack []int
+	var ans int
+	for i, h := range height {
+
+		for len(stack) > 0 && h > height[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				break
+			}
+			left := stack[len(stack)-1]
+			curWidth := i - left - 1
+			curHeight := min2(height[left], h) - height[top]
+			ans += curWidth * curHeight
+		}
+		stack = append(stack, i)
+	}
+	return ans
+}
+func min2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
