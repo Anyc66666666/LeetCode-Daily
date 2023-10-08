5268303cb426e81153bbf9d04fe5e10957e3c94b
Anyc66666666 <1662048992@qq.com>
2023-10-08 23:46:31 +0800

    💬2023/10/8-[84.]

---
+++ 2023/10/8-[84.].go
@@ -0,0 +1,81 @@
+package leetcode
+
+//给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+//
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+//
+//
+//
+// 示例 1:
+//
+//
+//
+//
+//输入：heights = [2,1,5,6,2,3]
+//输出：10
+//解释：最大的矩形为图中红色区域，面积为 10
+//
+//
+// 示例 2：
+//
+//
+//
+//
+//输入： heights = [2,4]
+//输出： 4
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= heights.length <=10⁵
+// 0 <= heights[i] <= 10⁴
+//
+//
+// Related Topics 栈 数组 单调栈 👍 2582 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func largestRectangleArea(heights []int) int {
+	n := len(heights)
+	left, right := make([]int, n), make([]int, n)
+	var stack []int
+	for i := 0; i < n; i++ {
+		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) == 0 {
+			left[i] = -1
+		} else {
+			left[i] = stack[len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	stack = []int{}
+	for i := n - 1; i >= 0; i-- {
+		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) == 0 {
+			right[i] = n
+		} else {
+			right[i] = stack[len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans = max1(ans, (right[i]-left[i]-1)*heights[i])
+	}
+	return ans
+}
+func max1(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+//在一维数组中对每一个数找到第一个比自己小的元素。
+//这类“在一维数组中找第一个满足某种条件的数”的场景就是典型的单调栈应用场景。
+//leetcode submit region end(Prohibit modification and deletion)
