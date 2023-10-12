e8b93b87768584e5022e60c7785c891d7ed42e1a
Anyc66666666 <1662048992@qq.com>
2023-10-12 23:31:56 +0800

    💬2023/10/12-[203.]

--- 2023/10/11-[1122.].go
+++ 2023/10/11-[1122.].go
@@ -1,7 +1,5 @@
 package leetcode
 
-import "sort"
-
 //给你两个数组，arr1 和 arr2，arr2 中的元素各不相同，arr2 中的每个元素都出现在 arr1 中。
 //
 // 对 arr1 中的元素进行排序，使 arr1 中项的相对顺序和 arr2 中的相对顺序相同。未在 arr2 中出现过的元素需要按照升序放在 arr1 的末
@@ -38,24 +36,69 @@ import "sort"
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func relativeSortArray(arr1 []int, arr2 []int) []int {
-	rank := map[int]int{}
-	for i, v := range arr2 {
-		rank[v] = i
+	//rank := map[int]int{}
+	//for i, v := range arr2 {
+	//	rank[v] = i
+	//}
+	//sort.Slice(arr1, func(i, j int) bool {
+	//	x, y := arr1[i], arr1[j]
+	//	rankX, hasX := rank[x]
+	//	rankY, hasY := rank[y]
+	//	if hasX && hasY {
+	//		return rankX < rankY
+	//	}
+	//	if hasX || hasY {
+	//		return hasX
+	//	}
+	//	return x < y
+	//})
+	//return arr1
+
+	upper := 0
+	for _, v := range arr1 {
+		if v > upper {
+			upper = v
+		}
 	}
-	sort.Slice(arr1, func(i, j int) bool {
-		x, y := arr1[i], arr1[j]
-		rankX, hasX := rank[x]
-		rankY, hasY := rank[y]
-		if hasX && hasY {
-			return rankX < rankY
+	frequency := make([]int, upper+1)
+
+	for _, v := range arr1 {
+		frequency[v]++
+	}
+
+	ans := make([]int, 0, len(arr1))
+
+	for _, v := range arr2 {
+		for ; frequency[v] > 0; frequency[v]-- {
+			ans = append(ans, v)
 		}
-		if hasX || hasY {
-			return hasX
+	}
+
+	for v, freq := range frequency {
+		for ; freq > 0; freq-- {
+			ans = append(ans, v)
 		}
-		return x < y
-	})
-	return arr1
+	}
+
+	return ans
 
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+
+/*
+注意到本题中元素的范围为 [0,1000]，这个范围不是很大，我们也可以考虑不基于比较的排序，例如「计数排序」。
+具体地，我们使用一个长度为 1001（下标从 0 到 10000）的数组frequency，
+记录每一个元素在数组 arr1 中出现的次数。随后我们遍历数组 arr2,
+当遍历到元素 x 时，我们将 frequency[x] 个 x 加入答案中，
+并将 frequency[x] 清零。当遍历结束后，所有在 arr2 中出现过的元素就已经有序了。
+此时还剩下没有在 arr2 中出现过的元素，
+因此我们还需要对整个数组 frequency 进行一次遍历。
+当遍历到元素 x 时，如果 frequency[x] 不为 0，我们就将 frequency[x] 个 x 加入答案中。
+
+细节
+我们可以对空间复杂度进行一个小优化。实际上，我们不需要使用长度为 1001 的数组，
+而是可以找出数组 arr1中的最大值 upper，
+使用长度为 upper+1 的数组即可。
+
+*/
