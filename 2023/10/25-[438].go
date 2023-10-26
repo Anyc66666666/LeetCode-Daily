61c1d66b74a67de064e10fb1f1c65d3f7dd3aca9
Anyc66666666 <1662048992@qq.com>
2023-10-26 23:59:44 +0800

    💬2023/10/26-[438.]

--- 2023/10/25-[438].go
+++ 2023/10/25-[438].go
@@ -40,19 +40,64 @@ package leetcode
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func findAnagrams(s string, p string) []int {
+	//var ans []int
+	//cnt:=[26]int{}
+	//for i:=0;i<len(p);i++{
+	//	cnt[p[i]-'a']++
+	//}
+	//for i:=0;i<=len(s)-len(p);i++{
+	//	cnt1:=[26]int{}
+	//	str:=s[i:i+len(p)]
+	//	for j:=0;j<len(str);j++{
+	//		cnt1[str[j]-'a']++
+	//	}
+	//	if cnt==cnt1{
+	//		ans=append(ans,i)
+	//	}
+	//}
+	//return ans
+
+	//-------------------------------
+
 	var ans []int
-	cnt := [26]int{}
-	for i := 0; i < len(p); i++ {
-		cnt[p[i]-'a']++
+	sLen, pLen := len(s), len(p)
+	if sLen < pLen {
+		return nil
 	}
-	for i := 0; i <= len(s)-len(p); i++ {
-		cnt1 := [26]int{}
-		str := s[i : i+len(p)]
-		for j := 0; j < len(str); j++ {
-			cnt1[str[j]-'a']++
+
+	count := [26]int{}
+	for i, ch := range p {
+		count[s[i]-'a']++
+		count[ch-'a']--
+	}
+	differ := 0
+	for _, c := range count {
+		if c != 0 {
+			differ++
 		}
-		if cnt == cnt1 {
-			ans = append(ans, i)
+	}
+
+	if differ == 0 {
+		ans = append(ans, 0)
+	}
+
+	for i, ch := range s[:sLen-pLen] {
+		if count[ch-'a'] == 1 {
+			differ--
+		} else if count[ch-'a'] == 0 {
+			differ++
+		}
+		count[ch-'a']--
+
+		if count[s[i+pLen]-'a'] == -1 {
+			differ--
+		} else if count[s[i+pLen]-'a'] == 0 {
+			differ++
+		}
+		count[s[i+pLen]-'a']++
+
+		if differ == 0 {
+			ans = append(ans, i+1)
 		}
 	}
 	return ans
