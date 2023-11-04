eba1148deece1fdba27f2262dc6a6ea81d929bdd
Anyc66666666 <1662048992@qq.com>
2023-11-04 23:59:21 +0800

    💬2023/11/4-[239.]

---
+++ 2023/11/4-[239.].go
@@ -0,0 +1,71 @@
+package leetcode
+
+//给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位
+//。
+//
+// 返回 滑动窗口中的最大值 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+//输出：[3,3,5,5,6,7]
+//解释：
+//滑动窗口的位置                最大值
+//---------------               -----
+//[1  3  -1] -3  5  3  6  7       3
+// 1 [3  -1  -3] 5  3  6  7       3
+// 1  3 [-1  -3  5] 3  6  7       5
+// 1  3  -1 [-3  5  3] 6  7       5
+// 1  3  -1  -3 [5  3  6] 7       6
+// 1  3  -1  -3  5 [3  6  7]      7
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1], k = 1
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10⁵
+// -10⁴ <= nums[i] <= 10⁴
+// 1 <= k <= nums.length
+//
+//
+// Related Topics 队列 数组 滑动窗口 单调队列 堆（优先队列） 👍 2591 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func maxSlidingWindow(nums []int, k int) []int {
+	var q []int
+	push := func(i int) {
+		for len(q) > 0 && nums[i] >= nums[q[len(q)-1]] {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
+	}
+
+	for i := 0; i < k; i++ {
+		push(i)
+	}
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q[0]]
+	for i := k; i < n; i++ {
+		push(i)
+		for q[0] <= i-k {
+			q = q[1:]
+		}
+		ans = append(ans, nums[q[0]])
+	}
+	return ans
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
