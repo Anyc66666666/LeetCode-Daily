7e6c68dc136fb266f3656b9a9faef07660987a41
Anyc66666666 <1662048992@qq.com>
2023-11-06 23:30:24 +0800

    💬2023/11/6-[347.]

--- 2023/11/5-[347.].go
+++ 2023/11/5-[347.].go
@@ -1,6 +1,6 @@
 package leetcode
 
-import "sort"
+import "container/heap"
 
 //给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
 //
@@ -37,22 +37,62 @@ import "sort"
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func topKFrequent(nums []int, k int) []int {
-	m := make(map[int]int)
-	for _, v := range nums {
-		m[v]++
+	//m:=make(map[int]int)
+	//for _,v:=range nums{
+	//	m[v]++
+	//}
+	//var a,ans []int
+	//for _,v:=range m{
+	//	a=append(a,v)
+	//}
+	//sort.Ints(a)
+	//n:=a[len(a)-k]
+	//for k,v:=range m{
+	//	if v>=n{
+	//		ans=append(ans,k)
+	//	}
+	//}
+	//return ans
+	//-----------------------------------
+	occurrences := map[int]int{}
+	for _, num := range nums {
+		occurrences[num]++
 	}
-	var a, ans []int
-	for _, v := range m {
-		a = append(a, v)
-	}
-	sort.Ints(a)
-	n := a[len(a)-k]
-	for k, v := range m {
-		if v >= n {
-			ans = append(ans, k)
+	h := &IHeap{}
+	heap.Init(h)
+	for key, v := range occurrences {
+		heap.Push(h, [2]int{key, v})
+		if h.Len() > k {
+			heap.Pop(h)
 		}
 	}
-	return ans
+	ret := make([]int, k)
+	for i := 0; i < k; i++ {
+		ret[k-1-i] = heap.Pop(h).([2]int)[0]
+	}
+	return ret
+}
+
+type IHeap [][2]int
+
+func (h IHeap) Len() int {
+	return len(h)
+}
+func (h IHeap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+func (h IHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *IHeap) Push(x any) {
+	*h = append(*h, x.([2]int))
+}
+func (h *IHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
