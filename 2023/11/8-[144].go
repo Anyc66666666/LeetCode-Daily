4d64664ec109d3b016ef3a6191fa1f865defd503
Anyc66666666 <1662048992@qq.com>
2023-11-08 23:58:20 +0800

    💬2023/11/8-[144]

---
+++ 2023/11/8-[144].go
@@ -0,0 +1,90 @@
+package leetcode
+
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,2,3]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+// 示例 4：
+//
+//
+//输入：root = [1,2]
+//输出：[1,2]
+//
+//
+// 示例 5：
+//
+//
+//输入：root = [1,null,2]
+//输出：[1,2]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1173 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var a []int
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		a = append(a, node.Val)
+
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return a
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
