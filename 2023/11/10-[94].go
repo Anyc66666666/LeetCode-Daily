f755266916ec40cbdc2cb498644048ed98a0b2d5
Anyc66666666 <1662048992@qq.com>
2023-11-10 23:51:30 +0800

    💬2023/11/10-[94]

---
+++ 2023/11/10-[94].go
@@ -0,0 +1,74 @@
+package leetcode
+
+//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1960 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+//type TreeNode struct {
+//	   Val int
+//	     Left *TreeNode
+//	   Right *TreeNode
+//	}
+
+func inorderTraversal(root *TreeNode) []int {
+	var a []int
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Left)
+		a = append(a, node.Val)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return a
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
