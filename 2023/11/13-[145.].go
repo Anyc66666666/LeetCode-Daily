479c665507e783180af16871a63d0e877a9aabc9
Anyc66666666 <1662048992@qq.com>
2023-11-13 23:53:22 +0800

    💬2023/11/13-[145.]

---
+++ 2023/11/13-[145.].go
@@ -0,0 +1,96 @@
+package leetcode
+
+//给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[3,2,1]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1117 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+//type TreeNode struct {
+//	  Val int
+//	  Left *TreeNode
+//	   Right *TreeNode
+//	}
+
+func postorderTraversal(root *TreeNode) []int {
+	//var a []int
+	//var dfs func(node *TreeNode)
+	//dfs = func(node *TreeNode) {
+	//	if node == nil {
+	//		return
+	//	}
+	//	dfs(node.Left)
+	//	dfs(node.Right)
+	//	a = append(a, node.Val)
+	//}
+	//dfs(root)
+	//return a
+
+	//------------------
+	var stk []*TreeNode
+	var res []int
+	var prev *TreeNode
+	for root != nil || len(stk) > 0 {
+		for root != nil {
+			stk = append(stk, root)
+			root = root.Left
+		}
+		root = stk[len(stk)-1]
+		stk = stk[:len(stk)-1]
+		if root.Right == nil || root.Right == prev {
+			res = append(res, root.Val)
+			prev = root
+			root = nil
+		} else {
+			stk = append(stk, root)
+			root = root.Right
+		}
+	}
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
