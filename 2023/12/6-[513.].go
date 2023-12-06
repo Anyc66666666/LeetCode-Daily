e065ff9e4c64602f25ea3cc98dc8d1907b38c7b2
Anyc66666666 <1662048992@qq.com>
2023-12-06 23:58:32 +0800

    💬2023/12/6-[513.]

---
+++ 2023/12/6-[513.].go
@@ -0,0 +1,73 @@
+package leetcode
+
+import "container/list"
+
+//给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
+//
+// 假设二叉树中至少有一个节点。
+//
+//
+//
+// 示例 1:
+//
+//
+//
+//
+//输入: root = [2,1,3]
+//输出: 1
+//
+//
+// 示例 2:
+//
+//
+//
+//
+//输入: [1,2,3,4,null,5,6,null,null,7]
+//输出: 7
+//
+//
+//
+//
+// 提示:
+//
+//
+// 二叉树的节点个数的范围是 [1,10⁴]
+//
+// -2³¹ <= Node.val <= 2³¹ - 1
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 547 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findBottomLeftValue(root *TreeNode) int {
+	queue := list.New()
+	var gradation int
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if i == 0 {
+				gradation = node.Val
+			}
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+	}
+	return gradation
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
