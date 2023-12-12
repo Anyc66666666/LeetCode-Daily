71b7d1e286359ba82d21c9925057ee7408eb66be
Anyc66666666 <1662048992@qq.com>
2023-12-12 23:40:03 +0800

    💬2023/12/12-[700]

--- 2023/12/11-[617.].go
+++ 2023/12/11-[617.].go
@@ -52,17 +52,71 @@ package leetcode
 //		    Right *TreeNode
 //		}
 func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	//if root1 == nil {
+	//	return root2
+	//}
+	//if root2 == nil {
+	//	return root1
+	//}
+	//root1.Val += root2.Val
+	//root1.Left = mergeTrees(root1.Left, root2.Left)
+	//root1.Right = mergeTrees(root1.Right, root2.Right)
+	//return root1
+
 	if root1 == nil {
 		return root2
 	}
 	if root2 == nil {
 		return root1
 	}
-	root1.Val += root2.Val
-	root1.Left = mergeTrees(root1.Left, root2.Left)
-	root1.Right = mergeTrees(root1.Right, root2.Right)
-	return root1
+	merged := &TreeNode{
+		Val: root1.Val + root2.Val,
+	}
+	queue := []*TreeNode{merged}
+	queue1 := []*TreeNode{root1}
+	queue2 := []*TreeNode{root2}
+	for len(queue1) > 0 && len(queue2) > 0 {
+		node := queue[0]
+		queue = queue[1:]
 
+		node1 := queue1[0]
+		queue1 = queue1[1:]
+
+		node2 := queue2[0]
+		queue2 = queue2[1:]
+
+		left1, right1 := node1.Left, node1.Right
+		left2, right2 := node2.Left, node2.Right
+		if left1 != nil || left2 != nil {
+			if left1 != nil && left2 != nil {
+				left := &TreeNode{Val: left1.Val + left2.Val}
+				node.Left = left
+				queue = append(queue, left)
+				queue1 = append(queue1, left1)
+				queue2 = append(queue2, left2)
+			} else if left1 != nil {
+				node.Left = left1
+			} else {
+				node.Left = left2
+			}
+		}
+
+		if right1 != nil || right2 != nil {
+			if right1 != nil && right2 != nil {
+				right := &TreeNode{Val: right1.Val + right2.Val}
+				node.Right = right
+				queue = append(queue, right)
+				queue1 = append(queue1, right1)
+				queue2 = append(queue2, right2)
+			} else if right1 != nil {
+				node.Right = right1
+			} else {
+				node.Right = right2
+			}
+		}
+
+	}
+	return merged
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ 2023/12/12-[700].go
@@ -0,0 +1,75 @@
+package leetcode
+
+//给定二叉搜索树（BST）的根节点
+// root 和一个整数值
+// val。
+//
+// 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回
+// null 。
+//
+//
+//
+// 示例 1:
+//
+//
+//
+//
+//
+//输入：root = [4,2,7,1,3], val = 2
+//输出：[2,1,3]
+//
+//
+// 示例 2:
+//
+//
+//输入：root = [4,2,7,1,3], val = 5
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数在 [1, 5000] 范围内
+// 1 <= Node.val <= 10⁷
+// root 是二叉搜索树
+// 1 <= val <= 10⁷
+//
+//
+// Related Topics 树 二叉搜索树 二叉树 👍 452 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+//	type TreeNode struct {
+//		    Val int
+//		    Left *TreeNode
+//		    Right *TreeNode
+//		}
+func searchBST(root *TreeNode, val int) *TreeNode {
+	var dfs func(node *TreeNode)
+	var ans *TreeNode
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		if node.Val == val {
+			ans = node
+		}
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return ans
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
