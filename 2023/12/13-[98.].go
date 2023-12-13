966c78dbf120fcadc7f33e89c18945478d8818f2
Anyc66666666 <1662048992@qq.com>
2023-12-13 23:56:41 +0800

    💬2023/12/13-[98.]

---
+++ 2023/12/13-[98.].go
@@ -0,0 +1,72 @@
+package leetcode
+
+import "math"
+
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+//
+// 有效 二叉搜索树定义如下：
+//
+//
+// 节点的左子树只包含 小于 当前节点的数。
+// 节点的右子树只包含 大于 当前节点的数。
+// 所有左子树和右子树自身必须也是二叉搜索树。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [2,1,3]
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目范围在[1, 10⁴] 内
+// -2³¹ <= Node.val <= 2³¹ - 1
+//
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 2234 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+//	type TreeNode struct {
+//		    Val int
+//		    Left *TreeNode
+//		    Right *TreeNode
+//		}
+func isValidBST(root *TreeNode) bool {
+	return helper(root, math.MinInt64, math.MaxInt64)
+}
+
+func helper(root *TreeNode, lower, upper int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+	return helper(root.Left, lower, root.Val) && helper(root.Right, root.Val, upper)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
