eaaff4bbc1d526e2bde0f2bf4fade13ca998d175
Anyc66666666 <1662048992@qq.com>
2024-01-01 23:59:53 +0800

    💬2024/1/1-[4]

---
+++ 2024/1/4-[1.].go
@@ -0,0 +1,100 @@
+package leetcode
+
+import "math"
+
+//给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+//
+// 算法的时间复杂度应该为 O(log (m+n)) 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums1 = [1,3], nums2 = [2]
+//输出：2.00000
+//解释：合并数组 = [1,2,3] ，中位数 2
+//
+//
+// 示例 2：
+//
+//
+//输入：nums1 = [1,2], nums2 = [3,4]
+//输出：2.50000
+//解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+//
+//
+//
+//
+//
+//
+// 提示：
+//
+//
+// nums1.length == m
+// nums2.length == n
+// 0 <= m <= 1000
+// 0 <= n <= 1000
+// 1 <= m + n <= 2000
+// -10⁶ <= nums1[i], nums2[i] <= 10⁶
+//
+//
+// Related Topics 数组 二分查找 分治 👍 6949 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	if len(nums1) > len(nums2) {
+		return findMedianSortedArrays(nums2, nums1)
+	}
+	m, n := len(nums1), len(nums2)
+	left, right := 0, m
+	median1, median2 := 0, 0
+	for left <= right {
+		i := (left + right) / 2
+		j := (m+n+1)/2 - i
+		nums_im1 := math.MinInt32
+		if i != 0 {
+			nums_im1 = nums1[i-1]
+		}
+		nums_i := math.MaxInt32
+		if i != m {
+			nums_i = nums1[i]
+		}
+		nums_jm1 := math.MinInt32
+		if j != 0 {
+			nums_jm1 = nums2[j-1]
+		}
+		nums_j := math.MaxInt32
+		if j != n {
+			nums_j = nums2[j]
+		}
+		if nums_im1 <= nums_j {
+			median1 = max(nums_im1, nums_jm1)
+			median2 = min(nums_i, nums_j)
+			left = i + 1
+		} else {
+			right = i - 1
+		}
+	}
+	if (m+n)%2 == 0 {
+		return float64(median1+median2) / 2.0
+	}
+	return float64(median1)
+
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
